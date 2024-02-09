b728321b94704526bfc3712ade34518fb63594b1
nmBPLG4413wQ <horpynych_ap@groupbwt.com>
2024-02-09 15:27:58 +0200

    feat: metadata lesson

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-# This is a sample Python script.
+from sqlalchemy import create_engine, text, Connection, MetaData, Table, Column, Integer, String, BigInteger, ForeignKey
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
+metadata = MetaData()
 
+user_table = Table(
+    "users",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("user_id", BigInteger, unique=True),
+    Column("fullname", String)
+)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+address = Table(
+    "addresses",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("user_id", ForeignKey('users.user_id')),
+    Column("email", String, nullable=False)
+)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+metadata.create_all(engine)
+metadata.drop_all(engine)
