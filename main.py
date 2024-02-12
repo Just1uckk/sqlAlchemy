3e2107be8d6bdb73662fa9f179a95adeb0a6fb07
nmBPLG4413wQ <horpynych_ap@groupbwt.com>
2024-02-12 11:26:53 +0200

    feat: and or and in operations

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, ForeignKey, insert
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, ForeignKey, insert, select, or_
 from sqlalchemy.dialects import postgresql, sqlite, mysql, oracle
 
 engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
@@ -9,8 +9,8 @@ user_table = Table(
     'users',
     metadata,
     Column('id', Integer, primary_key=True, unique=True, autoincrement=True),
-    Column('name', String(30)),
-    Column('fullname', String)
+    Column('first_name', String(30)),
+    Column('second_name', String)
 )
 
 address_table = Table(
@@ -23,23 +23,42 @@ address_table = Table(
 
 metadata.create_all(engine)
 
-stmt = insert(user_table).values(name='Test', fullname='Test Test')
-# print(stmt.compile(engine, sqlite.dialect()))
-# print(stmt.compile(engine, postgresql.dialect()))
-# print(stmt.compile(engine, mysql.dialect()))
-# print(stmt.compile(engine, oracle.dialect()))
-
-stmt_wo_values = insert(user_table)
-
-sqlite_stmt = stmt_wo_values.compile(engine, sqlite.dialect())
-postgresql_stmt = stmt_wo_values.compile(engine, postgresql.dialect())
-
 with engine.begin() as conn:
     result = conn.execute(
-        stmt_wo_values,
+        insert(user_table),
         [
-            {"name": "test1", "fullname": "test1 full"},
-            {"name": "test2", "fullname": "test2 full"},
-            {"name": "test3", "fullname": "test3 full"}
-         ]
+            {"first_name": "test1", "second_name": "test1 full"},
+            {"first_name": "test2", "second_name": "test2 full"},
+            {"first_name": "test3", "second_name": "test3 full"}
+        ]
     )
+
+with engine.begin() as conn:
+    # c - columns
+    result = conn.execute(
+        # Default operation
+        # select(user_table).where(user_table.c.name == "test1")
+
+        # AND operation
+
+        # select(user_table).where(
+        #     user_table.c.name.startswith("test"),
+        #     user_table.c.fullname.contains("3")
+        # )
+
+        # OR operation ( we can use _or or _and ... )
+        # Also we can set columns user_table.c.id
+
+        # select(user_table).where(
+        #     or_(user_table.c.name.startswith("test2"),
+        #     user_table.c.fullname.contains("3"))
+        # )
+
+        # IN operation
+        select(user_table.c.id).where(
+            user_table.c.id.in_([1,2])
+        )
+    )
+
+# mappings => []
+print(result.mappings().all())
