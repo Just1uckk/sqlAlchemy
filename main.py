1c5d3659bdf1d8c7cde42e304bc1f5bc0774a581
nmBPLG4413wQ <horpynych_ap@groupbwt.com>
2024-02-12 13:57:24 +0200

    feat: many to many relationship

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine,ForeignKey, select
 
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship, session, Session
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship, session, Session, joinedload
 
 engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
 
@@ -13,7 +13,7 @@ class User (Base):
     name: Mapped[str]
     age: Mapped[int]
     # uselist=False one-to-one relationship
-    addresses: Mapped[list["Address"]] = relationship(back_populates="user", uselist=True)
+    addresses: Mapped[list["Address"]] = relationship(back_populates="users", uselist=True, secondary="user_address")
 
     def __repr__(self) -> str:
         return f'User: {self.id=}:{self.name=}:{self.age=}'
@@ -22,25 +22,40 @@ class Address (Base):
     __tablename__ = 'addresses'
     email: Mapped[str] = mapped_column(primary_key=True)
     # uselist=False one-to-one relationship
-    user: Mapped["User"] = relationship(back_populates="addresses", uselist=False)
-    user_fk: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    users: Mapped[list["User"]] = relationship(back_populates="addresses", uselist=True, secondary="user_address")
+    # user_fk: Mapped[int] = mapped_column(ForeignKey('users.id'))
 
     def __repr__(self) -> str:
-        return f'Address: {self.email=}:{self.user_fk=}'
+        return f'Address: {self.email=}'
+
+class UserAdress(Base):
+    __tablename__ = 'user_address'
+    user_fk = mapped_column(ForeignKey('users.id'), primary_key=True)
+    address_fk = mapped_column(ForeignKey('addresses.email'), primary_key=True)
+
+    def __repr__(self) -> str:
+        return f'<UserAddress: {self.user_fk=}:{self.address_fk=}'
 
 Base.metadata.create_all(engine)
 
 session = Session(engine, expire_on_commit=True, autoflush=True)
 
-user = User(id=1, name="Test", age=30)
+user1 = User(id=1, name="Test1", age=30)
+user2 = User(id=2, name="Test2", age=20)
 address1 = Address(email="test1@test.com")
 address2 = Address(email="test2@test.com")
-user.addresses.append(address1)
-user.addresses.append(address2)
-session.add(user)
+user1.addresses.append(address1)
+user1.addresses.append(address2)
+user2.addresses.append(address1)
+user2.addresses.append(address2)
+session.add(user1)
+session.add(user2)
 session.commit()
 
-user = session.scalar(select(User))
+users = session.scalars(select(User)).all()
+addresses = session.scalars(select(Address)).all()
+user_secondary = session.scalars(select(UserAdress)).all()
 
-print(user)
-print(user.addresses)
+print(users)
+print(addresses)
+print(user_secondary)
