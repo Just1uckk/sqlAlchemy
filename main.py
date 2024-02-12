2121447e427ad3fa2aee0bc87417a0a8106e2581
nmBPLG4413wQ <horpynych_ap@groupbwt.com>
2024-02-12 11:49:09 +0200

    feat: methods select, order by, group by and others

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, ForeignKey, insert, select, or_
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, ForeignKey, insert, select, or_, desc
 from sqlalchemy.dialects import postgresql, sqlite, mysql, oracle
 
 engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
@@ -24,7 +24,7 @@ address_table = Table(
 metadata.create_all(engine)
 
 with engine.begin() as conn:
-    result = conn.execute(
+    conn.execute(
         insert(user_table),
         [
             {"first_name": "test1", "second_name": "test1 full"},
@@ -32,33 +32,38 @@ with engine.begin() as conn:
             {"first_name": "test3", "second_name": "test3 full"}
         ]
     )
-
+    conn.execute(
+        insert(address_table),
+        [
+            {"email_address": "test1@gmail.com", "user_id": 1},
+            {"email_address": "test2@gmail.com", "user_id": 2},
+            {"email_address": "test3@gmail.com", "user_id": 3}
+        ]
+    )
+# .label - set a key
 with engine.begin() as conn:
-    # c - columns
     result = conn.execute(
-        # Default operation
-        # select(user_table).where(user_table.c.name == "test1")
-
-        # AND operation
-
-        # select(user_table).where(
-        #     user_table.c.name.startswith("test"),
-        #     user_table.c.fullname.contains("3")
-        # )
-
-        # OR operation ( we can use _or or _and ... )
-        # Also we can set columns user_table.c.id
-
-        # select(user_table).where(
-        #     or_(user_table.c.name.startswith("test2"),
-        #     user_table.c.fullname.contains("3"))
-        # )
-
-        # IN operation
-        select(user_table.c.id).where(
-            user_table.c.id.in_([1,2])
+        select(
+            address_table.c.email_address.label('email'),
+            (user_table.c.first_name + ' ' + user_table.c.second_name).label('full_name')
+        ).where(
+            user_table.c.id > 0
+        )
+        # ).join_from(user_table, address_table, user_table.c.id == address_table.c.user_id)
+        .join(address_table, isouter=True)
+        .order_by(
+            # desc - back order
+            # desc(user_table.c.id)
+            # user_table.c.id.desc()
+            # or by name of field
+            "email"
+        )
+        # .group_by - grouping
+        .group_by(
+            "email"
         )
     )
 
-# mappings => []
-print(result.mappings().all())
+    print(result.all())
+
+
