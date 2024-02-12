ba23631b635e09faaceefc50c71133036b2b54b1
nmBPLG4413wQ <horpynych_ap@groupbwt.com>
2024-02-12 14:13:51 +0200

    feat: lazy-load strategies

--- main.py
+++ main.py
@@ -12,8 +12,7 @@ class User (Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str]
     age: Mapped[int]
-    # uselist=False one-to-one relationship
-    addresses: Mapped[list["Address"]] = relationship(back_populates="users", uselist=True, secondary="user_address")
+    addresses: Mapped[list["Address"]] = relationship(back_populates="user", uselist=True, lazy="selectin")
 
     def __repr__(self) -> str:
         return f'User: {self.id=}:{self.name=}:{self.age=}'
@@ -21,41 +20,32 @@ class User (Base):
 class Address (Base):
     __tablename__ = 'addresses'
     email: Mapped[str] = mapped_column(primary_key=True)
-    # uselist=False one-to-one relationship
-    users: Mapped[list["User"]] = relationship(back_populates="addresses", uselist=True, secondary="user_address")
-    # user_fk: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    user: Mapped["User"] = relationship(back_populates="addresses", uselist=False)
+    user_fk: Mapped[int] = mapped_column(ForeignKey('users.id'))
 
     def __repr__(self) -> str:
-        return f'Address: {self.email=}'
+        return f'Address: {self.email=}:{self.user_fk=}'
 
-class UserAdress(Base):
-    __tablename__ = 'user_address'
-    user_fk = mapped_column(ForeignKey('users.id'), primary_key=True)
-    address_fk = mapped_column(ForeignKey('addresses.email'), primary_key=True)
-
-    def __repr__(self) -> str:
-        return f'<UserAddress: {self.user_fk=}:{self.address_fk=}'
+# class UserAdress(Base):
+#     __tablename__ = 'user_address'
+#     user_fk = mapped_column(ForeignKey('users.id'), primary_key=True)
+#     address_fk = mapped_column(ForeignKey('addresses.email'), primary_key=True)
+#
+#     def __repr__(self) -> str:
+#         return f'<UserAddress: {self.user_fk=}:{self.address_fk=}'
 
 Base.metadata.create_all(engine)
 
 session = Session(engine, expire_on_commit=True, autoflush=True)
 
-user1 = User(id=1, name="Test1", age=30)
-user2 = User(id=2, name="Test2", age=20)
+user = User(id=1, name="Test1", age=30)
 address1 = Address(email="test1@test.com")
 address2 = Address(email="test2@test.com")
-user1.addresses.append(address1)
-user1.addresses.append(address2)
-user2.addresses.append(address1)
-user2.addresses.append(address2)
-session.add(user1)
-session.add(user2)
+user.addresses.append(address1)
+user.addresses.append(address2)
+session.add(user)
 session.commit()
 
-users = session.scalars(select(User)).all()
-addresses = session.scalars(select(Address)).all()
-user_secondary = session.scalars(select(UserAdress)).all()
-
-print(users)
-print(addresses)
-print(user_secondary)
+user = session.scalar(select(User))
+print(user)
+print(user.addresses)
